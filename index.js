76b1599fa31313597afca28493ad920d8a920a6d
Ravi-Ranjan-11 <93388492+Ravi-Ranjan-11@users.noreply.github.com>
2022-09-01 00:09:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${order.productName}</td>
+                        <td>${order.productNumber}</td>
+                        <td>${order.PaymentStatus}</td>
+                        <td class="$(order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending' ? 'warning': 'primary'}">${order.shipping}</td>
+                        <td class="primary"><a href="${order.details}" target="_blank">Details</a></td>
+                      `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,51 @@
+const Orders = [
+    {
+        productName: 'BMW M8 Coupé',
+        productNumber: '85631',
+        PaymentStatus: 'Due',
+        shipping: 'Pending',
+        details: 'https://www.bmw.in/en/all-models/m-series/m8-coupe/2020/bmw-m8-coupe-inspire.html'
+    },
+    {
+        productName: 'McLaren GT',
+        productNumber: '36378',
+        PaymentStatus: 'Refunded',
+        shipping: 'Declined',
+        details: 'https://cars.mclaren.com/en/new-mclaren-gt'
+    },
+    {
+        productName: 'Lamborghini Aventador',
+        productNumber: '49347',
+        PaymentStatus: 'Due',
+        shipping: 'Pending',
+        details: 'https://www.lamborghini.com/en-en/models/aventador'
+    },
+    {
+        productName: 'Bugatti Chiron',
+        productNumber: '96996',
+        PaymentStatus: 'Paid',
+        shipping: 'Delivered',
+        details: 'https://www.bugatti.com/chiron/'
+    },
+    {
+        productName: 'Jaguar I-Pace',
+        productNumber: '22821',
+        PaymentStatus: 'Paid',
+        shipping: 'Delivered',
+        details: 'https://www.jaguar.in/jaguar-range/i-pace/index.html'
+    },
+    {
+        productName: 'Ferrari Pininfarina Sergio',
+        productNumber: '81475',
+        PaymentStatus: 'Due',
+        shipping: 'Pending',
+        details: 'Ferrari Pininfarina Sergio'
+    },
+    {
+        productName: 'Audi RS5',
+        productNumber: '00485',
+        PaymentStatus: 'Paid',
+        shipping: 'Delivered',
+        details: 'https://www.audi.in/in/web/en/models/a5/rs-5-sportback-2021.html'
+    }
+]
